415890d0611e13082668e5dabe1bffa1b07f03ef
Svemat01 <jakob.helgesson@gmail.com>
2022-04-21 16:53:14 +0200

    Change blog file from .svx to .md

--- src/routes/posts/index.json.ts
+++ src/routes/posts/index.json.ts
@@ -4,7 +4,7 @@ import { dev } from "$app/env";
 import type { RequestHandler } from "@sveltejs/kit";
 
 export const get: RequestHandler = async () => {
-    const allPosts = import.meta.glob("./*.svx");
+    const allPosts = import.meta.glob("./*.md");
 
     const resolved = await Promise.all(
         Object.entries(allPosts)
