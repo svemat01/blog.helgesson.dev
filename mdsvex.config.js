814055e600c5e2612a740a91fc5e387bda938f6b
Svemat01 <jakob.helgesson@gmail.com>
2022-04-21 16:13:04 +0200

    Add custom containers

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,7 +1,8 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 
-import remarkGfm from "remark-gfm"
-import remarkSectionize from "remark-sectionize"
+import remarkGfm from 'remark-gfm';
+import remarkSectionize from 'remark-sectionize';
+import remarkContainers from 'remark-containers';
 
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
@@ -14,7 +15,54 @@ const config = defineConfig({
 
 	remarkPlugins: [
 		remarkGfm,
-		remarkSectionize
+		remarkSectionize,
+		[
+			remarkContainers,
+			{
+				default: true,
+				custom: [
+					{
+						type: 'warning',
+						element: 'div',
+						transform: function (node, config, tokenize) {
+							console.log({config});
+							node.data.hProperties = {
+								className: `remark-container warning ${config}` 
+							};
+							node.children.unshift({
+								type: 'p',
+								data: {
+									hName: 'p',
+									hProperties: {
+										className: 'title'
+									}
+								},
+								children: tokenize('WARNING')
+							});
+						}
+					},
+					{
+						type: 'tip',
+						element: 'div',
+						transform: function (node, config, tokenize) {
+							node.data.hProperties = {
+								className: `remark-container tip ${config}` 
+							};
+							node.children.unshift({
+								type: 'p',
+								data: {
+									hName: 'p',
+									hProperties: {
+										className: 'title'
+									}
+								},
+								children: tokenize('TIP')
+							});
+						}
+					}
+				]
+			}
+		]
 	],
 
 	rehypePlugins: []
