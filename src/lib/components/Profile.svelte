b479267b09707cef827e319da09fa39d3768e35b
Svemat01 <jakob.helgesson@gmail.com>
2022-04-22 00:22:39 +0200

    Profile Component

---
+++ src/lib/components/Profile.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Profiles, type Profile } from '$lib/data/profiles';
+
+	export let name: keyof typeof Profiles;
+
+	const profile = Profiles[name] as Profile | undefined;
+</script>
+
+{#if profile}
+	<span class="profile">
+		<img class="logo" src={profile.logo} alt={profile.displayName} />
+		<span class="profile-text">
+			{profile.displayName}
+		</span>
+	</span>
+{:else}
+	<span style="color: red"> USER NOT FOUND </span>
+{/if}
+
+<style lang="scss">
+	span.profile {
+		/* display: flex; */
+
+		color: $accent-color;
+
+		img.logo {
+			max-width: 2rem;
+			border-radius: 100%;
+		}
+	}
+</style>
---
+++ src/lib/data/profiles.ts
@@ -0,0 +1,25 @@
+export type Link = "github" | "linkedin" | "instagram" | "twitter" | "youtube" | "email" | "website"
+
+export type Profile = {
+    displayName: string;
+    description: string;
+    logo: string;
+
+    // links: Partial<Record<Link, string>>
+}
+
+
+export const Profiles: {
+    [k: string]: Profile;
+} = {
+    "luc": {
+        displayName: "Luc Van Kampen",
+        description: "The one and only",
+        logo: 'https://avatars.githubusercontent.com/u/10339043?v=4',
+    },
+    "antony": {
+        displayName: "Antonio F. Š",
+        description: "The Svelte Helper",
+        logo: 'https://media.antony.red/logoTransparent.png',
+    }
+}
