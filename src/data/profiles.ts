47192cf64170ea77eb971b181eb25e07f81f4151
Svemat01 <jakob.helgesson@gmail.com>
2022-08-12 00:50:33 +0200

    Start of posts

---
+++ src/data/profiles.ts
@@ -0,0 +1,12 @@
+export type Profile = {
+    name: string;
+}
+
+export const profiles: Record<string, Profile> = {
+    'luc': {
+        name: 'Luc Van Kampen',
+    },
+    'antony': {
+        name: 'Antonio F. Š'
+    }
+}
